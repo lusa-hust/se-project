cff085dfc898d7a545a1140c46e5af5707744d92
LuongNV <luongintel2009@gmail.com>
2017-01-01 13:35:35 +0700

    Add: set access control allow origin header

--- controllers/ViewController.js
+++ controllers/ViewController.js
@@ -6,6 +6,7 @@ var express = require('express');
 var ViewsController = express.Router();
 
 ViewsController.get('/', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "http://52.36.12.106");
     res.render('index.jade');
 });
 
