d25d418f703b2a88cd9a804486ea049a8e813b59
Sam Hoang <samhv.ict@gmail.com>
2017-01-17 18:15:27 +0700

    add controller for User

---
+++ controllers/AuthController.js
@@ -0,0 +1,146 @@
+'use strict'
+
+var express = require('express'),
+    User = require('../model/user'),
+    SALT_WORK_FACTOR = 10,
+    bcrypt = require('bcrypt');
+
+
+var DataValidator = require('./Helper/DataValidator');
+var DV = new DataValidator();
+
+
+var AuthController = express.Router();
+
+var loginFunction = function (req, res) {
+    if (req.body.token !== undefined) {
+        User.findOne({'token': req.body.token}, function (err, person) {
+            if (err || person == null) {
+                res.json({
+                    status: false,
+                    error: {
+                        token: 'token not found'
+                    },
+                    err: err
+                });
+                return;
+            }
+
+            res.json({
+                status: true,
+                account: person
+            })
+
+        });
+
+        return;
+    }
+
+
+    if (DV.emailValidator(req.body.email) == false) {
+        res.json({
+            status: false,
+            error: "Invalid Email"
+        });
+
+        return;
+    }
+
+
+    User.findOne({'email': req.body.email}, function (err, person) {
+        if (err || person == null) {
+            res.json({
+                status: false,
+                error: {
+                    email: 'Email Not Found'
+                }
+            });
+            return;
+        }
+
+        person.comparePassword(req.body.password, function (err, isMatch) {
+            if (err) throw err;
+
+            if (isMatch) {
+                // generate a salt
+                bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+                    if (err) return next(err);
+
+                    // hash the password along with our new salt
+                    bcrypt.hash(person.email + person.password, salt, function (err, hash) {
+                        if (err) return next(err);
+
+                        // override the cleartext password with the hashed one
+                        person.token = hash;
+                        person.save();
+
+                        res.json({
+                            status: true,
+                            account: person.toJSON(),
+                            token: person.token
+                        })
+                    });
+                });
+
+                return;
+            }
+
+            res.json({
+                status: false,
+                error: {
+                    password: 'password not match'
+                }
+            })
+        });
+    });
+};
+
+var signUpFunction = function (req, res) {
+    var error = {};
+
+    if (DV.nameValidator(req.body.name) == false) {
+        error.name = "Invalid name";
+    }
+
+    if (DV.emailValidator(req.body.email) == false) {
+        error.email = "Invalid Email";
+    }
+
+    if (Object.keys(error).length != 0) {
+
+        res.json({
+            status: false,
+            error: error
+        });
+        return;
+    }
+
+    var newUser = new User();
+
+    newUser.name = req.body.name;
+
+    newUser.email = req.body.email;
+
+    newUser.password = req.body.password;
+
+    newUser.save(function (err) {
+        if (err) {
+            res.status(500).json({
+                status: false,
+                error: "Can not create user because of database error !"
+            });
+            return;
+        }
+
+        res.status(201).json({
+            status: true,
+            message: 'User created!'
+        });
+
+    });
+};
+
+AuthController.post('/login', loginFunction);
+AuthController.post('/signup', signUpFunction);
+
+module.exports = AuthController;
---
+++ controllers/UsersRestController.js
@@ -0,0 +1,204 @@
+/**
+ * Created by samhv on 11/28/16.
+ */
+
+'use strict'
+
+var express = require('express');
+var User = require('../model/user');
+
+var DataValidator = require('./Helper/DataValidator');
+var DV = new DataValidator();
+
+var UsersRestController = express.Router();
+
+var addUser = function (req, res) {
+    // Create new user
+
+    var error = {};
+
+    if (DV.nameValidator(req.body.name) == false) {
+        error.name = "Invalid name";
+    }
+
+    if (DV.emailValidator(req.body.email) == false) {
+        error.email = "Invalid Email";
+    }
+
+    if (Object.keys(error).length != 0) {
+
+        res.json({
+            status: false,
+            error: error
+        });
+        return;
+    }
+
+    var newUser = new User();
+
+    newUser.name = req.body.name;
+
+    newUser.email = req.body.email;
+
+    newUser.password = req.body.password;
+
+    if (req.body.admin != undefined) {
+        newUser.admin = req.body.admin;
+    }
+
+    newUser.save(function (err) {
+        if (err) {
+            res.status(500).json({
+                status: false,
+                error: "Can not create user because of database error !"
+            });
+            return;
+        }
+
+        res.status(201).json({
+            status: true,
+            message: 'User created!'
+        });
+
+    });
+
+};
+
+
+var getUserById = function (req, res) {
+    // get user by id
+    User.findById(req.params.id, function (err, user) {
+        if (err) {
+            res.status(500).json({
+                status: false,
+                error: "Can not find user because of database error !"
+            });
+            return;
+        }
+        res.json({
+            status: true,
+            user: user
+        });
+
+    });
+};
+
+var editUserById = function (req, res) {
+    // remove user by id
+    User.findById(req.params.id, function (err, user) {
+
+        if (err) {
+            res.status(500).json({
+                status: false,
+                error: "Can not remove user because of database error !"
+            });
+            return;
+        }
+
+        for (var k in req.body) {
+            user[k] = req.body[k]
+        }
+
+        user.save(function (err) {
+            if (err) {
+                res.status(500).json({
+                    status: false,
+                    error: "Can not edit user because of database error !"
+                });
+                return;
+            }
+
+            res.status(200).json({
+                status: true,
+                message: 'User update!',
+            });
+
+        });
+
+    });
+};
+
+var removeUserById = function (req, res) {
+    // remove user by id
+    User.remove({
+        _id: req.params.id
+    }, function (err, user) {
+        if (err) {
+            res.status(500).json({
+                status: false,
+                error: "Can not remove user because of database error !"
+            });
+            return;
+        }
+
+        res.json({
+            status: true,
+            message: 'User Successfully deleted'
+        });
+
+    });
+};
+
+var getAllUser = function (req, res) {
+    // get all user
+    User.find(function (err, users) {
+        if (err) {
+            res.status(500).json({
+                status: false,
+                error: "Can not get list of user because of database error !"
+            });
+            return;
+        }
+        res.json({
+            status: true,
+            users: users
+        });
+
+    });
+};
+
+var checkAuth = function (req, res, next) {
+
+    if (req.headers.token !== undefined) {
+        User.findOne({'token': req.headers.token}, function (err, person) {
+            if (err || person == null) {
+                res.status(403).json({
+                    status: false,
+                    error: "Forbidden need Admin permission"
+                });
+                return;
+            }
+
+            if (person.admin == true)
+                next();
+            else if (req.params.id != undefined) {
+                if (req.params.id == person._id)
+                    next();
+            } else {
+                res.status(403).json({
+                    status: false,
+                    error: "Forbidden need Admin permission"
+                });
+            }
+
+        });
+    } else {
+        res.status(403).json({
+            status: false,
+            error: "Forbidden need Admin permission"
+        });
+    }
+}
+UsersRestController.use('/', checkAuth);
+
+UsersRestController.post('/user', addUser);
+
+UsersRestController.get('/user/:id', getUserById);
+
+UsersRestController.put('/user/:id', editUserById);
+
+UsersRestController.delete('/user/:id', removeUserById);
+
+UsersRestController.get('/users', getAllUser);
+
+module.exports = UsersRestController;
