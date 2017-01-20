26f7804569f550acb2d2f8f4156c6a2c6bae754b
LuongNV <luongintel2009@gmail.com>
2017-01-20 10:25:27 +0700

    Add: tracking

--- public/js/word.js
+++ public/js/word.js
@@ -1,7 +1,6 @@
 function getInputAndLookup() {
   var word = $('input[name="word"]').val();
   lookup(word);
-  getHistory();
 }
 
 function lookup(word) {
@@ -16,6 +15,11 @@ function lookup(word) {
       // if fetch successfully
       if (data.found) {
         // if the word is found
+        if (localStorage.getItem("token")) {
+          // if we have token
+          getHistory();
+        }
+
         displayWord(data.word);
         // set the current lookup word
         app.word = word;
@@ -42,7 +46,29 @@ function getHistory() {
 }
 
 function showTrackingList(data) {
-  console.log(data);
+  var html = '';
+  if (data) {
+    if (data.status) {
+      var list = data.trackingList.list;
+      console.log(list);
+      for (var item in list) {
+        if (list.hasOwnProperty(item)) {
+          html += '<div class="history-item clearfix">';
+          html += '<div class="history-word">' + item;
+          html += '</div><div class="history-count">' + list[item] +'</div></div>';
+        }
+      }
+      if (html) {
+        $('.history-container').css({"display": "block"});
+        $('.history-body').html(html);
+        if (list.hasOwnProperty(app.word) && list[app.word] > 1) {
+          $('.tracking-times').html('You looked up this word ' + list[app.word] + ' times');
+        }
+      } else {
+        $('.history-container').css({"display": "none"});
+      }
+    }
+  }
 }
 
 function displayWord(word) {
